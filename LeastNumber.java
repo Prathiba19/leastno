91aac0d0f9866291edfcc3bbc4ab0311b0bd2773
Prathiba19 <prathibaksr@gmail.com>
2016-08-08 14:52:40 +0530

    Create LeastNumber.java

---
+++ LeastNumber.java
@@ -0,0 +1,54 @@
+# leastno
+class LeastNumber {
+    public int firstDecreasing;
+    public int nextStart;
+}
+
+public static String getLeastNumberDeletingDigits_2(String number, int k) {
+    String leastNumber = number;
+    int start = 0;
+    while(k > 0 && leastNumber.length() > 0) {
+    	LeastNumber  result = getLeastNumber (leastNumber, start);
+        if(result.firstDecreasing >= 0) {
+            leastNumber = removeDigit(leastNumber, result.firstDecreasing);
+        }
+        else {
+            leastNumber = removeDigit(leastNumber, leastNumber.length() - 1);
+        }
+
+        start = result.nextStart;
+        --k;
+    }
+
+    return leastNumber;
+}
+
+private static LeastNumber  getNextDecreasing(String number, int start) {
+    int firstDecreasing = -1;
+    int nextStart;
+
+    for(int i = start; i < number.length() - 1; ++i) {
+        int curDigit = number.charAt(i) - '0';
+        int nextDigit = number.charAt(i + 1) - '0';
+        if(curDigit > nextDigit) {
+            firstDecreasing = i;
+            break;
+        }
+    }
+
+    if(firstDecreasing == 0) {
+        nextStart = 0;
+    }
+    else if (firstDecreasing > 0) {
+        nextStart = firstDecreasing - 1;
+    }
+    else {
+        nextStart = number.length();
+    }
+
+    LeastNumber  result = new LeastNumber ();
+    result.firstDecreasing = firstDecreasing;
+    result.nextStart = nextStart;
+
+    return result;
+}}
